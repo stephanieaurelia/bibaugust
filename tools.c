0e763249253a0eba9d7df8cdac06625f91ac61fe
Cyntiahp31 <89081126+Cyntiahp31@users.noreply.github.com>
2021-09-05 20:37:31 +0800

    Update tools.c

--- tools.c
+++ tools.c
@@ -15,6 +15,43 @@ void hapus_buku(int n, char *filename)
         }
         i++;
     }
+struct bukuTambahan{
+    char judul[1000];
+    int tahunTerbit;
+    char penulis[500];
+    int jumlahHalaman;
+    double beratBuku;
+    float ratingBuku;
+    long long int ISBN;
+    char availability[10];
+};
+    
+void tambahBuku (){
+    struct bukuTambahan buku;
+
+    scanf("%[^\n]", buku.judul);
+    getchar();
+    scanf("%d", &buku.tahunTerbit);
+    getchar();
+    scanf("%[^\n]", &buku.penulis);
+    getchar();
+    scanf("%d", &buku.jumlahHalaman);
+    getchar();
+    scanf("%lf", &buku.beratBuku);
+    getchar();
+	scanf("%f", &buku.ratingBuku);
+    getchar();
+    scanf("%lld", &buku.ISBN);
+    getchar();
+    scanf("%s", buku.availability);
+    getchar();
+
+    FILE *fAppend = fopen("database.txt", "a");         // menambahkan input user ke dalam database.txt
+    fprintf(fAppend, "%s#%d#%s#%d#%lf#%f#%lld#%s\n", buku.judul, buku.tahunTerbit, buku.penulis,
+            buku.jumlahHalaman, buku.beratBuku, buku.ratingBuku, buku.ISBN,
+            &buku.availability);
+    fclose(fAppend);
+}
 
     fclose(fp);
     fclose(tmpfile);
